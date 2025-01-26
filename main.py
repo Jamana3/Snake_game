299d1e9fc20c7efded6f73af3901df42d0901f9b
Jamana3 <e3jamna141592@gmail.com>
2025-01-27 00:45:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import turtle
 import time
 from food import Food              ### if we only write import food then we would not able to use class Food so we need
-from turtle import Screen
-### to import Food class also from food module
+from turtle import Screen          ### to import Food class also from food module                        
 from snake import Snake
 from scoreboad import Score
 
@@ -30,7 +29,7 @@ food = Food()
 game_is_on = True
 while game_is_on:
     snake.move()
-    screen.update()                     ### whatever code we have written yet gonna see only after we update our screen.
+    screen.update()                     ### whatever code is written, gonna see only after we update our screen.
     time.sleep(0.1)                     ###  the screen got sleep for 0.2 seconds.
     if snake.object[0].distance(food)<15:
         food.create()
